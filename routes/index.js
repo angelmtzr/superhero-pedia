fa6025f2d9efccaefd24c5f2cbe0a6328506642e
angelmtzr <angel.mtz.r02@gmail.com>
2021-11-08 13:19:16 -0600

    f

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,26 @@ let currentId = 1;
 
 // INDEX ROUTE
 router.route('/')
-    .get((req,res) => {
+    .get((req, res) => {
         currentId = 1;
         axios.get(URL + currentId.toString())
             .then((apiResponse) => {
                 const character = apiResponse.data;
                 res.render('character', {character: character});
+            });
+    })
+    .post((req, res) => {
+        searchName = req.body.searchName;
+        axios.get(URL + "search/" + searchName)
+            .then((apiResponse) => {
+                try{
+                    const character = apiResponse.data.results[0];
+                    currentId = parseInt(character.id);
+                    res.redirect('/' + currentId.toString());
+                }
+                catch{
+                    res.redirect('/' + currentId.toString());
+                }
             });
     });
 
